8ed1a0e30c8cca177ba9ce094cd5f83f28f2b5dc
jackson Croy <Jrcroy0607@gmail.com>
2020-04-10 00:46:23 -0700

    Updates

---
+++ precache-manifest.dd069d77d77493c5e2d945405e3b2cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6fefc085a97b24a7b6a3fc89aa7073",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8a5bfe26ca18c81503a3",
+    "url": "/react-portfolio/static/css/main.2a776c17.chunk.css"
+  },
+  {
+    "revision": "5224037f032d3b9605cc",
+    "url": "/react-portfolio/static/js/2.93e74835.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.93e74835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5bfe26ca18c81503a3",
+    "url": "/react-portfolio/static/js/main.4e099b4d.chunk.js"
+  },
+  {
+    "revision": "26950ce1f34a1b315732",
+    "url": "/react-portfolio/static/js/runtime-main.e44c21d0.js"
+  }
+]);
